7155aff76e9cce072c5c3c012adaa7ac17c609f9
SuperPro228 <104450154+SuperPro228@users.noreply.github.com>
2026-07-31 20:10:14 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// ===== КОНФИГУРАЦИЯ FIREBASE (из твоего скриншота) =====
+const firebaseConfig = {
+    apiKey: "AIzaSyBepxuzaf56wyH_Mxaz9ZRr8Pn-qKAd2ks",
+    authDomain: "vibe-messenger-6a05c.firebaseapp.com",
+    projectId: "vibe-messenger-6a05c",
+    storageBucket: "vibe-messenger-6a05c.firebasestorage.app",
+    messagingSenderId: "623283246823",
+    appId: "1:623283246823:web:475f2bc68ee1193828f442",
+    measurementId: "G-702RZ4CKNZ"
+};
+
+// ===== ИНИЦИАЛИЗАЦИЯ =====
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
